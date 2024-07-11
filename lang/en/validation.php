e29960f9da7fb76786c6c06a375e212757705ac2
Ilyes512 <ilyes.ahidar@gmail.com>
2024-07-11 15:15:39 +0200

    Removed config dir and added lang dir

---
+++ lang/en/validation.php
@@ -0,0 +1,7 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'enum_names' => 'The selected ":Attribute" is invalid.',
+];
--- src/EnumUtilsServiceProvider.php
+++ src/EnumUtilsServiceProvider.php
@@ -9,24 +9,18 @@ use Illuminate\Support\ServiceProvider;
 class EnumUtilsServiceProvider extends ServiceProvider
 {
     /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        $this->mergeConfigFrom(__DIR__ . '/../config/enum_utils.php', 'enum_utils');
-    }
-
-    /**
      * Bootstrap any application services.
      */
     public function boot(): void
     {
+        $this->loadTranslationsFrom(__DIR__ . '/../lang', 'enumUtils');
+
         if ($this->app->runningInConsole()) {
             return;
         }
 
         $this->publishes([
-            __DIR__ . '/../config/enum_utils.php' => $this->app->configPath('enum_utils.php'),
-        ], 'enum-utils-config');
+            __DIR__ . '/../lang' => $this->app->langPath('vendor/enumUtils'),
+        ]);
     }
 }
